67d91f7b7b19b1c69e0bbad2348dd5f884b765a6
Gabriel Sá <147980001+gabrielsadesouza@users.noreply.github.com>
2024-11-05 00:42:45 -0300

    front com todas as telas

---
+++ Pages/04 - Meta Financeira/scriptMeta.js
@@ -0,0 +1,4 @@
+// Função para adicionar funcionalidade ao botão "Adicionar Meta"
+document.querySelector(".add-button").addEventListener("click", function() {
+    alert("Aqui você pode adicionar uma nova meta financeira!");
+});
